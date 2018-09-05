fd2df05bc989400c718b221b50e861bf323d050b
Darag <mohameddarag1996@gmail.com>
2018-09-05 16:14:41 +0200

    Create Add a Gig Form

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,15 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+
+namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public DateTime Date { get; set; }
+        public DateTime Time { get; set; }
+        public int Genre { get; set; }
+        public IEnumerable<Genre> Genres { get; set; }
+    }
+}
